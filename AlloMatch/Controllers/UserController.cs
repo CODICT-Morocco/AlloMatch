08eb4423f4348ed86ec2cb112d9e6535760a18d0
zHaytam <haytam.zanid@gmail.com>
2021-02-24 13:55:56 +0100

    Add profile endpoint

--- AlloMatch/Controllers/UserController.cs
+++ AlloMatch/Controllers/UserController.cs
@@ -1,5 +1,6 @@
 ﻿using AlloMatch.DTOs;
 using AlloMatch.Services;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.ComponentModel.DataAnnotations;
 using System.Threading.Tasks;
@@ -11,10 +12,12 @@ namespace AlloMatch.Controllers
     public class UserController : ControllerBase
     {
         private readonly IUserService _userService;
+        private readonly ICurrentUserService _currentUserService;
 
-        public UserController(IUserService userService)
+        public UserController(IUserService userService, ICurrentUserService currentUserService)
         {
             _userService = userService;
+            _currentUserService = currentUserService;
         }
 
         [HttpPost("login")]
@@ -26,6 +29,11 @@ namespace AlloMatch.Controllers
 
             return Response<AccessTokenDto>.Success(result);
         }
+
+        [Authorize]
+        [HttpGet("profile")]
+        public async Task<Response<UserProfileDto>> Profile()
+            => Response<UserProfileDto>.Success(await _userService.GetUserInfo(_currentUserService.UserId));
     }
 
     public record LoginRequestDto([Required][EmailAddress] string Email, [Required] string Password);
